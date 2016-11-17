f41dbbdc86b80b84b06a1bfa5df1688fe497438b
dazhu666 <827489423@qq.com>
2016-11-17 14:59:12 +0800

    配置springMvc

---
+++ project/src/com/project/controller/IndexController.java
@@ -0,0 +1,18 @@
+package com.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@RequestMapping("/index")
+@Controller
+public class IndexController {
+
+	@RequestMapping("/index")
+	public ModelAndView index(){
+		ModelAndView modelAndView=new ModelAndView();
+		System.out.println("qqq");
+		modelAndView.setViewName("index");
+		return modelAndView;
+	}
+}
